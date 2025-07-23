50cd616aa4277f695821749c9ed6718ef631d24f
Sanskar <sanskarghule04@gmail.com>
2025-07-23 10:02:29 +0000

    latest

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "s3bucket" {
         Environment = "Dev"
     }
 }
+
+resource "aws_s3_bucket" "s3bucket12" {
+    bucket = "awsterra04200204bucket"
+    acl    = "private"
+ 
+    tags = {
+        Name        = "bucketterra"
+        Environment = "Dev"
+    }
+}
